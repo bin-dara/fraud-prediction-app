8681c23fae4d409ee84e17a844cfd25c8a43cc70
bin-dara <bindarad@booleandata.ai>
2025-07-07 18:21:55 +0530

    Update Accelerator.py

--- Accelerator.py
+++ Accelerator.py
@@ -182,6 +182,7 @@ st.plotly_chart(pie_fig, use_container_width=True)
     # ---------------------------
     # Basic validation
     # ---------------------------
+st.subheader("Pie Chart - Amount Vs Transaction type")
 if "Transaction_type" in df.columns and "Amount" in df.columns:
         # Group by Transaction Type
         summary = df.groupby("Transaction_type")["Amount"].sum().reset_index()
